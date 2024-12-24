8d109cdc6c30b37687b8abfbeaeaeb9461c15b05
Long Le <long.le21062004@gmail.com>
2024-12-24 17:16:13 +0200

    Leetcode 49

--- Arrays & Hashing/49GroupAnagrams.js
+++ Arrays & Hashing/49GroupAnagrams.js
@@ -3,13 +3,17 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-  var res = {};
-  var str = '';
-  var len = strs.length;
-  for (var i = 0; i < len; i++) {
-    str = Array.from(strs[i]).sort().join('');
-    if (!res[str]) res[str] = [];
-    res[str].push(strs[i]);
+  const res = {};
+  for (let s of strs) {
+      const count = new Array(26).fill(0);
+      for (let c of s) {
+          count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+      }
+      const key = count.join(',');
+      if (!res[key]) {
+          res[key] = [];
+      }
+      res[key].push(s);
   }
   return Object.values(res);
 };
