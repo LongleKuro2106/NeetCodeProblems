2b5092c0109529560306002342037c5908c83ab0
Long Le <long.le21062004@gmail.com>
2024-12-24 14:26:29 +0200

    Leetcode 242

--- Arrays & Hashing/242ValidAnagram.js
+++ Arrays & Hashing/242ValidAnagram.js
@@ -4,18 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  if (s.length !== t.length) {
-      return false;
-  }
-  let sMap = {}
-  for (let i = 0; i< s.length; i++) {
-      sMap[s[i]] = (sMap[s[i]] || 0) + 1;
-  }
-  for (let i = 0; i < t.length; i++) {
-      if(!sMap[t[i]]) {
-          return false;
-      }
-      sMap[t[i]]--;
-  }
-  return true;
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const countS = {};
+    const countT = {};
+    for (let i = 0; i < s.length; i++) {
+        countS[s[i]] = (countS[s[i]] || 0) + 1;
+            countT[t[i]] = (countT[t[i]] || 0) + 1;
+        }
+
+    for (const key in countS) {
+        if (countS[key] !== countT[key]) {
+            return false;
+        }
+    }
+    return true;
 };
