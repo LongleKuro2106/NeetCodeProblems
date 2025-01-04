0e64488199e29f58d234326b2be57f3174e5ede6
Long Le <long.le21062004@gmail.com>
2025-01-04 09:21:18 +0200

    Leetcode 20

---
+++ Stack/22GenerateParentheses.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  const res = Array.from({ length: n + 1 }, () => []);
+  res[0] = [""];
+
+  for (let k = 0; k <= n; k++) {
+      for (let i = 0; i < k; i++) {
+          for (const left of res[i]) {
+              for (const right of res[k - i - 1]) {
+                  res[k].push("(" + left + ")" + right);
+              }
+          }
+      }
+  }
+  return res[n];
+};
