36ba9b3296a73f3a0bb6e488194229c072b856b8
Long Le <long.le21062004@gmail.com>
2025-02-04 12:03:27 +0200

    Leetcode 226

---
+++ Trees/226InvertBinaryTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (root === null) return null;
+
+  const node = new TreeNode(root.val);
+
+  node.right = invertTree(root.left);
+  node.left = invertTree(root.right);
+
+  return node;
+};
